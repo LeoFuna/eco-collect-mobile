8b136a82dfcf5d1e4d34d8024d2cbb4f2e399a7e
leofuna <leo.funa@gmail.com>
2023-09-16 07:56:19 -0300

    feat: app component initial thoughts

--- App.tsx
+++ App.tsx
@@ -1,20 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { GestureHandlerRootView } from  'react-native-gesture-handler';
+import { StatusBar, StyleSheet } from 'react-native';
+import * as SplashScreen from 'expo-splash-screen';
+import { Roboto_400Regular, Roboto_500Medium } from '@expo-google-fonts/roboto';
+import { Ubuntu_700Bold, useFonts } from '@expo-google-fonts/ubuntu';
+import { useCallback } from 'react';
+import Routes from './src/routes';
+
+// SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_500Medium, Ubuntu_700Bold });
+
+  // // https://docs.expo.dev/versions/latest/sdk/splash-screen/
+  // const onLayoutRootView = useCallback(async () => {
+  //   if (fontsLoaded) {
+  //     await SplashScreen.hideAsync();
+  //   }
+  // }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      {/* <View onLayout={onLayoutRootView}> */}
+          <GestureHandlerRootView style={styles.container}>
+            <StatusBar barStyle='dark-content' backgroundColor='transparent' translucent />
+            <Routes />
+          </GestureHandlerRootView>
+      {/* </View> */}
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
